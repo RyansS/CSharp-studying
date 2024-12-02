15f049c1c2a26172a538f95ead0a569f1cff4d6d
Ryan Silva <monteiroryan2016@gmail.com>
2024-12-02 13:12:06 -0300

    study

---
+++ Temperature/Temperature/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+using TemperatureIs; // The namespace can't have the same name of the class, it will cause a problem.
+
+Console.WriteLine("How is the temperature today?");
+int degree = int.Parse(Console.ReadLine());
+
+Temperature.weather(degree); // it works like a normal method, i just call the class first
+
+Console.ReadKey();
+
