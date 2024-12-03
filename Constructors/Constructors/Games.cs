360b2488072fb112d19a6485e506b514365d79cc
Ryan Silva <monteiroryan2016@gmail.com>
2024-12-03 13:48:46 -0300

    objects and constructors

---
+++ Constructors/Constructors/Games.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Games
+    {
+
+        public Games(string name, int price) // this is the constructor his function is automactilly assign the values of objects
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public string name; // we create the caracteristics of our objects
+        public int price;   // *important* we have to set the fields and method to public to acess them in the main program
+
+        public void buy() // we create the actions that can happen with our games
+
+        //here we will buy the game
+        {
+            Console.WriteLine("You will buy " + name + " for " + price + "$");
+        }
+        public void play() // we create the actions that can happen with our games
+
+        //here we will play the game
+        {
+            Console.WriteLine("You will play " + name);
+        }
+    }
+}
