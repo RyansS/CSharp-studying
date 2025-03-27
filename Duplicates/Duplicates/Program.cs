86515d200a4517b9bfdf0ba1dd775cc07ba37638
Ryan Silva <monteiroryan2016@gmail.com>
2025-03-27 16:36:25 -0300

    Students class and Duplicate check

---
+++ Duplicates/Duplicates/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int [] numbers =    //this variable stores the numbers created by the method generate()
+        generate();     
+        find(numbers);
+    }
+
+    public static void find(int[] numbs)
+    {
+        for (int i = 0; i < numbs.Length; i++)
+        {
+            for (int j = i + 1; j < numbs.Length; j++) //basically the j is the number in front of the i number
+            {
+                if (numbs[j] == numbs[i]) //so if the number[j] is the same as the number[i] it's a duplicate number and
+                                          //it's gonna pass through all the array
+                {
+                    Console.WriteLine(numbs[i] +" is a duplicate number");
+                }
+            }
+        }
+    }
+    public static int[] generate() //it's a int[] method, so we can return the array numbs[] with no problems
+    {
+        Random filler = new Random(); //creating a new random
+
+        int[] numbs = new int[10]; //array of the numbs limited to 10
+
+        for (int i = 0; i < numbs.Length; i++)
+        {
+            int fil = filler.Next(1, 10); //this int variable is gonna have the random number from 1 through 10 stored
+                                           // also, its inside the first for because we want different numbers, so it's gonna give us always different
+                                           //numbers when it executes
+                for (int k = 0; k < numbs.Length; k++) //the second loop will store the random numbers in the array of numbs[]
+                {
+                    numbs[i] = fil;
+                }
+
+        }
+
+        Console.WriteLine("Here's the generated numbers:"); //showing the numbers
+            foreach (int j in numbs)
+            {
+                Console.WriteLine(j +" ");
+            }
+
+        return numbs; //it's gonna return the numbers generated
+    }
+}
---
+++ Students/Students/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    public class student
+    {
+        public string name;
+        public int year;
+        public double grade;
+
+        public student (string name, int year, double grade)
+        {
+            this.name = name;
+            this.year = year;
+            this.grade = grade;
+        }
+
+        public void study ()
+        {
+            Console.WriteLine(name + " from the *"+ year + " is studying");
+        }
+        public void obgrade()
+        {
+            Console.WriteLine(name + " got " + grade + " in the grade");
+        }
+    }
+}
