88c941e490d0f14a5d38e4894ebc5eb733c8d21e
Ryan Silva <monteiroryan2016@gmail.com>
2025-04-11 14:01:46 -0300

    Static Modifier & Get and Set

---
+++ Static modifier/Static modifier/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        weapons.show();
+        Console.ReadKey();
+
+        }
+    }
+
+class weapons
+{
+    public string name;
+
+    public weapons(string name)
+    {
+        this.name = name;
+    }
+
+    public static void show() 
+{
+        Console.WriteLine("WEaponssss!");
+}
+
+}
