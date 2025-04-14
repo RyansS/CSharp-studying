6243be361083f8e193bc3d6ebfbcd1cae5957cdd
Ryan Silva <monteiroryan2016@gmail.com>
2025-04-14 16:55:27 -0300

    frequency2

---
+++ Frequency2/Frequency2/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num1 = 0;
+        int num2 = 0;
+
+        int result = 0;
+
+        Console.WriteLine("1st nmb:");
+        num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("2nd nmb:");
+        num2 = int.Parse(Console.ReadLine());
+
+        result = num1 + num2;
+
+        Console.WriteLine(result);
+
+        Console.ReadKey();
+    }
+}
