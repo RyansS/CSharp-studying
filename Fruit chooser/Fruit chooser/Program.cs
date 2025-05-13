93fb42ac28a93d6bfb51adc7ce939169af0dbed3
Ryan Silva <monteiroryan2016@gmail.com>
2025-05-13 13:35:56 -0300

    Fruit chooser

---
+++ Fruit chooser/Fruit chooser/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+
+
+        Console.ReadKey();
+    }
+}
+
+class Fruit
+{
+    private int _counter = 0;
+    private string type;
+    private int id;
+
+    public string Type
+    {
+        get { return type; }
+        set { type = value; }
+    }
+
+    public Fruit ()
+    {
+        this.
+    }
+}
