4a67ecc58bc30079d0072baeda24f58a5e6513b1
Ryan Silva <monteiroryan2016@gmail.com>
2025-05-15 00:01:05 -0300

    bank

---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = 0;
+
+        int[] values = {100, 50, 20, 10, 5, 2, 1};
+
+
+        Console.WriteLine("How much money?/n");
+        n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            n/values[i];
+        }
+
+        Console.ReadKey();
+    }
+        
+    }
