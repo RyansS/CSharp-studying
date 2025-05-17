0beec875c86f4e6e3e0f7d5484fd7c00e326d0dd
Ryan Silva <monteiroryan2016@gmail.com>
2025-05-17 13:29:16 -0300

    fruitupdate

--- Fruit chooser/Fruit chooser/Program.cs
+++ Fruit chooser/Fruit chooser/Program.cs
@@ -5,7 +5,6 @@
 
 
 
-
         Console.ReadKey();
     }
 }
@@ -15,6 +14,7 @@ class Fruit
     private int _counter = 0;
     private string type;
     private int id;
+    private double price;
 
     public string Type
     {
@@ -22,8 +22,27 @@ class Fruit
         set { type = value; }
     }
 
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public double Price
+    {
+        get { return price; }
+        set { price = value; }
+    }
+
     public Fruit ()
     {
-        this.
+        this.Type = type;
+        this.Id = id;
+        this.Price = price;
+    }
+
+    public static void BuyFruit ()
+    {
+        Console.WriteLine();
     }
 }
