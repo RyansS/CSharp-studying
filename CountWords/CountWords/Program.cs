3ee6e6fcaf21052782d9aa6940b5c4c03096aab0
Ryan Silva <monteiroryan2016@gmail.com>
2025-05-30 15:42:08 -0300

    cc

---
+++ CountWords/CountWords/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+    static void CountWord ()
+    {
+        Console.Write("Write a phrase: ");
+        string phrase = Console.ReadLine();
+        string[] words = phrase.Split(' ');
+
+        Dictionary<string,int> allwords = new Dictionary<string, int>();
+
+        foreach (string word in words) {
+            
+    }
+
+    static void Title(string title)
+    {
+        int CaracTitle = title.Length;
+        string Asterict = title.PadLeft(CaracTitle, '*');
+        Console.WriteLine(Asterict);
+        Console.WriteLine(title);
+        Console.WriteLine(Asterict);
+    }
+}
