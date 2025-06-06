18b5329ffe8cb88b39fa07f96cf09778d8353396
Ryan Silva <monteiroryan2016@gmail.com>
2025-06-06 15:24:47 -0300

    BookCatalog

---
+++ BookCatalog/BookCatalog/BookCatalog.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Book
+    {
+        
+
+        public string ISBN;
+        public string title;
+        public string author;
+        public int numbOfPages;
+        
+        public Book (string ISBN, string title, string author, int numbOfPages)
+        {
+            this.ISBN = ISBN;
+            this.title = title;
+            this.author = author;
+            this.numbOfPages = numbOfPages;
+        }
+
+        public override string ToString()
+        {
+            return $"Title: {title}\n Author: {author}\n Pages: {numbOfPages}\n ISBN: {ISBN}";
+        }
+
+
+    }
+
+    class BookCatalog
+    {
+        private Dictionary<string, Book> catalog = new Dictionary<string, Book>();
+
+        public void AddBook(Book newBook)
+        {
+            catalog[newBook.ISBN] = newBook;
+        }
+
+        public static Book CreateBook()
+        {
+            Console.Clear();
+
+            Console.Write("Which book are you going to add? ");
+            var title = Console.ReadLine();
+
+            Console.Write("\nWhat's it's author? ");
+            var author = Console.ReadLine();
+
+            Random numbPages = new();
+            int numbOfPages = numbPages.Next(20, 500);
+
+            Random ISBNrandom = new();
+            int ISBNint = ISBNrandom.Next(100000, 900000);
+            string ISBN = Convert.ToString(ISBNint);
+
+            Book newBook = CreateBook();
+            return new Book(ISBN, title, author, numbOfPages);
+        }
+
+        public void ShowBooks()
+        {
+            Console.Clear();
+            Console.WriteLine($"Total of title in our catalog: {catalog.Count}");
+
+            foreach (var allbooks in catalog.Values)
+            {
+                allbooks.ToString();
+            }
+        }
+    }
+
+}
---
+++ BookCatalog/BookCatalog/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using Library;
+using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var catalog1 = new BookCatalog();
+        Menu("Welcome to the library!");
+        
+    }
+    
+
+    public void DoTitle (string title)
+    {
+        int quantity = title.Length;
+        string symbolTitle = title.PadLeft(quantity, '*');
+        Console.WriteLine(symbolTitle);
+        Console.WriteLine(title);
+        Console.WriteLine(symbolTitle + "\n");
+    }
+
+    public static void Menu(string v)
+    {
+        Console.Clear();
+        Console.WriteLine("\n");
+        Console.WriteLine("1 - Show books");
+        Console.WriteLine("2 - Add a book");
+        Console.WriteLine("3 - Remove a book");
+        Console.WriteLine("4 - Search book");
+        Console.WriteLine("5 - Exit");
+
+        Console.Write("\n");
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1":
+                
+                break;
+            case "2": break;
+            case "3": break;
+            case "4": break;
+            case "5": break;
+        }
+
+    }
+
+
+}
