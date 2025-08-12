c68d4bdd10546699755a293219c7d2ab8b48a5af
RyansS <monteiroryan2016@gmail.com>
2025-08-12 11:58:22 -0300

    Register Function on Steam Project

---
+++ SteamProject/SteamProject/Program.cs
@@ -0,0 +1,34 @@
+﻿
+Console.WriteLine("Welcome to steam!");
+
+static void Register ()
+{
+    bool repeat = false;
+
+    while (repeat)
+    {
+
+        Console.Write("How we should call you? ");
+        string username = Console.ReadLine();
+
+        Console.WriteLine("Now we need some crucial informations from you ");
+
+        Console.Write("What's your email? ");
+        string email = Console.ReadLine();
+
+        Console.Write("\nNow create a strong password: ");
+        string password = Console.ReadLine();
+
+        if (username != null && email != null && password != null)
+        {
+            Console.WriteLine("\nThank you to joined the Steam universe!");
+            repeat = false;
+        }
+        else
+        {
+            Console.WriteLine("\nPlease, keep sure that you've typed you're" +
+                " credentials properly");
+            repeat = true;
+        }
+    }
+} 
