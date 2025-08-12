e6b7c9763977d8e693dd793693165dbbe9b04e84
RyansS <monteiroryan2016@gmail.com>
2025-08-12 11:39:57 -0300

    ConnectingDatabase

---
+++ ConnectingDatabase/ConnectingDatabase/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using ConnectingDatabase.Database;
+
+
+
+try
+{
+    using var connection = new Connection().openConnection();
+    connection.Open();
+    Console.WriteLine(connection.State);
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+return;
