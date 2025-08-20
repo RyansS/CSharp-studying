b42c7ffc1ce1e3cbcfd79ebc829d7712cbd6b407
RyansS <monteiroryan2016@gmail.com>
2025-08-19 23:34:06 -0300

    calculator

---
+++ Cam/Cam/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.WriteLine("value 1: ");
+int value1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("value 2: ");
+int value2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("operator: ");
+string op = Console.ReadLine();
+
+Console.WriteLine(FunctionCalculate(value1,value2,op));
+
+
+int FunctionCalculate (int value1, int value2, string op)
+{
+    int result = 0;
+
+    switch (op)
+    {
+        case "+":
+                result = value1 + value2;
+                break;
+
+        case "-":
+            result = value1 - value2;
+            break;
+
+        case "*":
+            result = value1 * value2;
+            break;
+
+        case "/":
+            result = value1 / value2;
+            break;
+    }
+
+    return result;
+}
