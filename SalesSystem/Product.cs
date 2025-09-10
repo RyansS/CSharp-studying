0b56cac9524466c5c5264ec8039892bee9ccbf33
RyansS <monteiroryan2016@gmail.com>
2025-09-10 13:00:32 -0300

    did some fixes

--- SalesSystem/Product.cs
+++ SalesSystem/Product.cs
@@ -1,9 +1,8 @@
 using System.Text.Json.Serialization;
-
 class Product
 {
     [JsonPropertyName("title")]
-    public string? Title { get; set; }
+    public string Title { get; set; }
 
     [JsonPropertyName("normalPrice")]
     public double Price { get; set; }
@@ -13,4 +12,17 @@ class Product
 
     [JsonPropertyName("internalName")]
     public string InternalName { get; set; }
+
+    [JsonPropertyName("steamRatingCount")]
+    public int? NumberOfSells { get; set; }
+
+
+    public static void SeeAllProducts(List<Product> listFromAPI)
+    {
+        Program.CreateTitle("All Products");
+
+        foreach (var product in listFromAPI) {
+            Console.WriteLine($"Title: {product.Title}, Price: {product.Price}, ID: {product.GameId}");
+        }
+    }
 }
