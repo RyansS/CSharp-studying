9bb90ab7d5ff34c6f18b4d85de031ab4e4d0bbf1
RyansS <monteiroryan2016@gmail.com>
2025-09-11 12:08:09 -0300

    Now it's all on the same folder

--- SteamProjectW/obj/Debug/net9.0/SteamProjectW.AssemblyInfo.cs
+++ SteamProjectW/obj/Debug/net9.0/SteamProjectW.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SteamProjectW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b14d00b6406a76aeb57a9e695c20c99404fdd390")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b56cac9524466c5c5264ec8039892bee9ccbf33")]
 [assembly: System.Reflection.AssemblyProductAttribute("SteamProjectW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SteamProjectW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
