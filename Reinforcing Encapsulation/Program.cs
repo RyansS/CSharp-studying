2da0ba5b0c4a248e76a32509e9543116a0c9b03b
RyansS <monteiroryan2016@gmail.com>
2025-10-13 10:08:18 -0300

    Almost finishing

--- Reinforcing Encapsulation/Program.cs
+++ Reinforcing Encapsulation/Program.cs
@@ -2,7 +2,9 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        
+
+        
     }
 }
 
@@ -15,6 +17,48 @@ class Product
     public string Name { get; }
     public decimal Price { get; private set; }
     public int Quantity { get; private set; }
+
+    public void Menu()
+    {
+        bool repeatCode = true;
+
+        CreateTitle("Menu");
+        Console.WriteLine("1- Show Product");
+        Console.WriteLine("2- Change Price");
+        Console.WriteLine("3- Manage Stock");
+        Console.WriteLine("0- Exit");
+
+        int answer = int.Parse(Console.ReadLine());
+
+        switch (answer)
+        {
+            case 0:
+                repeatCode = false;
+                break;
+
+            case 1:
+                
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+        }
+    }
+
+    public void CreateTitle(string title)
+    {
+        int numbOfCharacters = title.Length;
+        string symbol = string.Empty.PadLeft(numbOfCharacters, '-');
+
+        Console.WriteLine(symbol);
+        Console.WriteLine(title);
+        Console.WriteLine(symbol);
+    }
 
     public Product(string name, decimal price, int quantity)
     {
