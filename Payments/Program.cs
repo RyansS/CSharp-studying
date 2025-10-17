e597634e3ae825ae04849917c8dc3d0a73eda912
RyansS <monteiroryan2016@gmail.com>
2025-10-17 13:49:27 -0300

    Payment code 1

---
+++ Payments/Program.cs
@@ -0,0 +1,53 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var processor = new PaymentProcessor();
+
+        processor.ProcessPayment(new PaypalPayment(), 100.0m);
+    }
+}
+
+public interface IPayment
+{
+    void Process(decimal amount);
+}
+
+public class PaypalPayment
+{
+    public void Process (decimal amount)
+    {
+        Console.WriteLine($"Registering payment of the total: {amount}$");
+    }
+}
+
+public class CreditCardPayment
+{
+    public void Process (decimal amount)
+    {
+        Console.WriteLine($"Registering payment of the total: {amount}$");
+    }
+}
+
+public class PixPayment
+{
+    public void Process(decimal amount)
+    {
+        Console.WriteLine($"Registering payment of the total: {amount}$");
+    }
+}
+
+public class PaymentProcessor
+{
+    public void ProcessPayment(IPayment payment, decimal amount)
+    {
+        payment.Process(amount);
+    }
+
+    internal void ProcessPayment(PaypalPayment paypalPayment, decimal v)
+    {
+        throw new NotImplementedException();
+    }
+}
+
