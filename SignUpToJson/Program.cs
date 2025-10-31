bbe4ac0ae7b0641eab0c671dc6778c630fb37059
RyansS <monteiroryan2016@gmail.com>
2025-10-31 15:22:20 -0300

    Did the generate Json method

--- SignUpToJson/Program.cs
+++ SignUpToJson/Program.cs
@@ -1,8 +1,13 @@
-﻿internal class Program
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-
+        UtilityMethod.Users.Add(SignUp.SignUpMethod());
+        SelectedUsers.GenerateJson();
 
         Console.ReadKey();
     }
@@ -28,26 +33,43 @@ class SignUp
     public string Name { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public SignUp (string name, string email, string password)
+    public SignUp(string name, string email, string password)
     {
         Name = name;
         Email = email;
         Password = password;
     }
-    public static SignUp SignUpMethod() {
+
+
+    public static SignUp SignUpMethod()
+    {
 
         UtilityMethod.CreateTitle("Welcome to sign up!");
 
-            Console.WriteLine("Username: ");
-            string usernick = Console.ReadLine();
+        Console.WriteLine("Username: ");
+        string usernick = Console.ReadLine();
 
-            Console.WriteLine("Email: ");
-            string email = Console.ReadLine();
+        Console.WriteLine("Email: ");
+        string email = Console.ReadLine();
 
-            Console.WriteLine("Password: ");
+        Console.WriteLine("Password: ");
         string password = Console.ReadLine();
 
         return new SignUp(usernick, email, password);
+    }
+
+}
+
+class SelectedUsers
+{
+
+    public static void GenerateJson ()
+    {
+        string FileValue = JsonSerializer.Serialize(UtilityMethod.Users);
+
+        File.WriteAllText("users-archive-info.json", FileValue);
+
+        Console.WriteLine("The Json file was created successfully!");
     }
     
 }
