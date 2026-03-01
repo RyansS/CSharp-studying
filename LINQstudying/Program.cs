637cb71f3a301b027671bdf262fef89b6ec5edcf
RyansS <monteiroryan2016@gmail.com>
2026-03-01 12:55:45 -0300

    Linq method syntax

--- LINQstudying/Program.cs
+++ LINQstudying/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> numbers = new List<int> {1, 3, 4, 1, 7 ,6 ,7 ,8, 4, 3};
+
+// LINQ Query Syntax
+
+var FilterNumbersQ = from num in numbers //
+where num == 7 || num < 8
+select num;
+
+foreach (var shownumbs in FilterNumbersQ)
+{
+    Console.WriteLine(shownumbs);
+}
+
+// LINQ Method Syntax
+
+var FilterNumbersM = numbers.Where(num => num > 6 || num < 4).Distinct(); // Can use two or more methods in a single line
+
+
