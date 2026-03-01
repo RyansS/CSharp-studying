6c6a44d1f112f39b1dd31f6f4d88cf0864c56dc6
RyansS <monteiroryan2016@gmail.com>
2026-03-01 14:02:05 -0300

    Cities program

---
+++ Cities/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Best cities to visit in the world!!");
+
+class City
+{
+    public string Name {get; set;}
+    public int Population {get; set;}
+    public string Mayor {get; set;}
+
+    public City (string name, int population, string mayor)
+    {
+        Name = name;
+        Population = population;
+        Mayor = mayor;
+    }
+
+    Dictionary<string, string> Cities = new Dictionary<string, string>
+    {
+        {"USA", "Washington"},
+        {"IE", "Dublin"},
+        {"BR", "Brasília"}
+    };
+
+    public static void Menu (Dictionary<string, string> Cities)
+    {
+        foreach(KeyValuePair<string, string> showCities in Cities)
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine($"- {showCities.Key}, {showCities.Value}");
+            Console.WriteLine("------------------");
+        }
+    }
+}
