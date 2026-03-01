4b10071b02b5f658f57dbe97b1ac25e2c8e90dd7
RyansS <monteiroryan2016@gmail.com>
2026-03-01 13:09:56 -0300

    Dictionaries

---
+++ Dictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿Dictionary<string, string> Cities = new Dictionary<string, string>
+{
+    {"India", "Mumbai"}, // You could add data like this either
+    { "USA", "New York" }
+};
+
+Cities.Add("UK", "London"); // Easier way to do it
+Cities.Add("IE", "Dublin");
+
+string AccessingCity = Cities["UK"]; // Store value in a variable
+
+Cities["India"] = "Nova Dehli"; // Update value
+
+Cities.Remove("IE"); // As long as I remove the key it'll remove the value as well
+
+foreach (KeyValuePair<string, string> dictionary in Cities) //this is the dictionary type (just to know)
+{
+    Console.WriteLine($"- Country: {dictionary.Key}; City: {dictionary.Value}");
+}
