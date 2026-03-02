217222d7079fcdd2679c4dd28150232cc21a96a8
RyansS <monteiroryan2016@gmail.com>
2026-03-02 11:23:59 -0300

    Finished Withdraw Method

--- BankAccount2/Program.cs
+++ BankAccount2/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.ReadKey();
+    }
+
+    class BankAccount
+    {
+        public string Username {get; private set;}
+
+        public decimal Balance {get; private set;}
+
+        private int IdUser {get; set;}
+
+        List <BankAccount> AllBankAccounts = new List<BankAccount>();
+
+        public BankAccount (string username, decimal balance, int iduser)
+        {
+            Username = username;
+            Balance = balance;
+            IdUser = iduser;
+        }
+
+        public void Withdraw () {
+            
+            Console.WriteLine("- Please, say your name: ");
+            string NameAnswer = Console.ReadLine();
+
+            Console.WriteLine("- Please, write your id: ");
+            int IdAnswer = int.Parse(Console.ReadLine());
+
+            var SearchAccount = AllBankAccounts.Where(match => match.Username == NameAnswer && match.IdUser == IdAnswer);
+
+            if (SearchAccount != null)
+            {
+                Console.WriteLine($"- Your balance: {Balance}");
+                Console.Write("How much would you like to withdraw? ");
+                decimal Ammount = decimal.Parse(Console.ReadLine());
+
+                Balance -= Ammount;
+
+                Console.WriteLine($"- Withdrawing {Ammount}$...");
+                Console.ReadKey();
+            }
+        }
+    }
+
+
+}
