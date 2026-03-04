0a871bfbd4f2336175595535a77c6ca7b3a4aa39
RyansS <monteiroryan2016@gmail.com>
2026-03-04 10:57:42 -0300

    Finished base class

---
+++ RegisterFileRead/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("- Welcome to Steam -");
+
+class User
+{
+    public string Username {get; private set;}
+
+    public string Email {get; private set;} 
+
+    public string Password {get; private set;}
+
+    public int Age {get; set;}
+
+
+    public User (string username, string email, string password, int age)
+    {
+        Username = username;
+        Email = email;
+        Password = password;
+        Age = age;
+    }
+}
