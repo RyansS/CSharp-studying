e184ffc90ebd471d807c6a8404c59467fa7f1bc1
RyansS <monteiroryan2016@gmail.com>
2026-03-04 11:27:41 -0300

    Register method

--- RegisterFileRead/Program.cs
+++ RegisterFileRead/Program.cs
@@ -18,4 +18,42 @@ class User
         Password = password;
         Age = age;
     }
+
+    public User Register () {
+        int AgeInput;
+        string UserInput;
+        string EmailInput;
+        string PasswordInput;
+
+
+        Console.WriteLine("- Register -\n");
+
+        while (true)
+        {
+        Console.Write("- Username: ");
+        UserInput = Console.ReadLine();
+
+        Console.Write("- Email: ");
+        EmailInput = Console.ReadLine();
+
+        Console.Write("- Password: ");
+        PasswordInput= Console.ReadLine();
+
+        Console.Write("- Age: ");
+        int.TryParse(Console.ReadLine(), out AgeInput);
+
+        if (UserInput.Length >= 5 && EmailInput.Length >= 10 && PasswordInput.Length > 7) 
+            {
+                break;
+            }
+        }
+
+
+        return new User (UserInput, EmailInput, PasswordInput, AgeInput);
+
+    }
+
+    public void Login () {
+
+    }
 }
